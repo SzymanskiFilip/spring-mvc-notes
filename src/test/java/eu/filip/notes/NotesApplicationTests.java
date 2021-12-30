cb86cab60a3c855da6278072d13f0c04028627a1
Filip <szymanski.fp@gmail.com>
2021-12-30 16:00:04 +0100

    initialized project

---
+++ src/test/java/eu/filip/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.filip.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
