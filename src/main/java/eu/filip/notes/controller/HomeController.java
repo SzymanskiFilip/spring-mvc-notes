b8fbb404b6becad3084de18c010d3b9166975603
Filip <szymanski.fp@gmail.com>
2021-12-31 00:08:07 +0100

    created a / endpoint that returns home model with the text attribute of hello!

---
+++ src/main/java/eu/filip/notes/controller/HomeController.java
@@ -0,0 +1,14 @@
+package eu.filip.notes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("text", "hello!");
+        return "home";
+    }
+}
