57a66542a5029f93b86158ca4d5e82314765797a
Filip <szymanski.fp@gmail.com>
2021-12-31 15:03:55 +0100

    changed constructor to @AllArgsConstructor

--- src/main/java/eu/filip/notes/model/Note.java
+++ src/main/java/eu/filip/notes/model/Note.java
@@ -1,20 +1,16 @@
 package eu.filip.notes.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Note {
     private int user_id;
     private int id;
     private String content;
-
-    public Note(int user_id, int id, String content){
-        this.user_id = user_id;
-        this.id = id;
-        this.content = content;
-    }
 
     public Note(String content){
         this.content = content;
