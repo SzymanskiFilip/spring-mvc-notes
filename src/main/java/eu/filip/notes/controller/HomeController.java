9ace66f251c9b5a565beba6c27422380e15937ba
Filip <szymanski.fp@gmail.com>
2021-12-31 15:00:51 +0100

    added logging on endpoint hit

--- src/main/java/eu/filip/notes/controller/HomeController.java
+++ src/main/java/eu/filip/notes/controller/HomeController.java
@@ -1,13 +1,16 @@
 package eu.filip.notes.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class HomeController {
     @GetMapping("/")
     public String home(Model model){
+        log.info("/ - ENDPOINT HIT");
         model.addAttribute("text", "hello!");
         return "home";
     }
