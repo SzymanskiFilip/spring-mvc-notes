1842486af0fef2277722f18b90f4e12971687999
Filip <szymanski.fp@gmail.com>
2022-01-01 00:51:07 +0100

    fixed note entity by changing id property to Long

--- src/main/java/eu/filip/notes/model/Note.java
+++ src/main/java/eu/filip/notes/model/Note.java
@@ -4,12 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(name = "notes")
 public class Note {
-    private int user_id;
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long user_id;
     private String content;
 
     public Note(String content){
