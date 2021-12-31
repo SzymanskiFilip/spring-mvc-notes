0ef3f15952d77b977fe586573869524c72c7a869
Filip <szymanski.fp@gmail.com>
2022-01-01 00:50:05 +0100

    created User entity

--- src/main/java/eu/filip/notes/model/User.java
+++ src/main/java/eu/filip/notes/model/User.java
@@ -1,2 +1,23 @@
-package eu.filip.notes.model;public class User {
+package eu.filip.notes.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String first_name;
+    private String last_name;
 }
