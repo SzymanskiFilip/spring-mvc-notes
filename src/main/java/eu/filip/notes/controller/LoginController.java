65b8a63d52d326c468c6e5c50dab7947771ac639
Filip <szymanski.fp@gmail.com>
2021-12-31 15:01:38 +0100

    returning cutom login page on login endpoint

--- src/main/java/eu/filip/notes/controller/LoginController.java
+++ src/main/java/eu/filip/notes/controller/LoginController.java
@@ -1,2 +1,15 @@
-package eu.filip.notes.controller;public class LoginController {
+package eu.filip.notes.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        log.info("/login - ENDPOINT HIT");
+        return "login";
+    }
 }
