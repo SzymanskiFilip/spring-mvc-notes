10936ec757963ef6af5319d7a1b6a749847a9ee7
Filip <szymanski.fp@gmail.com>
2022-01-01 00:51:43 +0100

    added user repo with finaAll method

--- src/main/java/eu/filip/notes/service/UserRepository.java
+++ src/main/java/eu/filip/notes/service/UserRepository.java
@@ -1,2 +1,10 @@
-package eu.filip.notes.service;public interface UserRepository {
+package eu.filip.notes.service;
+
+import eu.filip.notes.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    ArrayList<User> findAll();
 }
