076e297b6ca680f6c316b38e70a897bba8ae560c
Filip <szymanski.fp@gmail.com>
2021-12-31 15:02:38 +0100

    added /notes that returns dummy notes, GET /add that returns add page, POST /add that logs new note

--- src/main/java/eu/filip/notes/controller/NotesController.java
+++ src/main/java/eu/filip/notes/controller/NotesController.java
@@ -1,15 +1,32 @@
 package eu.filip.notes.controller;
 
 import eu.filip.notes.model.Note;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class NotesController {
     @GetMapping("/notes")
     public String notes(Model model){
+        log.info("/ - ENDPOINT HIT");
         model.addAttribute("note", new Note(2,2, "My First  note!"));
         return "notes";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model){
+        log.info("/add - ENDPOINT HIT");
+        model.addAttribute("note", new Note());
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String add(Note note){
+        log.info("added new note:" + note.toString());
+        return "home";
     }
 }
