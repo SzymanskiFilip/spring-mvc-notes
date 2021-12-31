46c8b2bf2edc2ebdd1ca9c32ea7c9a00ce0c8e30
Filip <szymanski.fp@gmail.com>
2022-01-01 00:51:31 +0100

    added Notes repo with findAll method

--- src/main/java/eu/filip/notes/service/NotesRepository.java
+++ src/main/java/eu/filip/notes/service/NotesRepository.java
@@ -1,2 +1,10 @@
-package eu.filip.notes.service;public interface NotesRepository {
+package eu.filip.notes.service;
+
+import eu.filip.notes.model.Note;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.ArrayList;
+
+public interface NotesRepository extends CrudRepository<Note, Long> {
+    ArrayList<Note> findAll();
 }
