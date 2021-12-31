1de63d79aec9f0e0692c1b1c7dc528e79a03ee61
Filip <szymanski.fp@gmail.com>
2022-01-01 00:49:45 +0100

    added test controller to test repos

--- src/main/java/eu/filip/notes/controller/TestController.java
+++ src/main/java/eu/filip/notes/controller/TestController.java
@@ -1,2 +1,33 @@
-package eu.filip.notes.controller;public class TestController {
+package eu.filip.notes.controller;
+
+import eu.filip.notes.model.Note;
+import eu.filip.notes.model.User;
+import eu.filip.notes.service.NotesRepository;
+import eu.filip.notes.service.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class TestController {
+
+    private NotesRepository notesRepository;
+    private UserRepository userRepository;
+
+    public TestController(NotesRepository notesRepository, UserRepository userRepository) {
+        this.notesRepository = notesRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/testN")
+    public ArrayList<Note> test(){
+        return notesRepository.findAll();
+    }
+
+    @GetMapping("/testU")
+    public ArrayList<User> users(){
+        return userRepository.findAll();
+    }
 }
