980abcc868417d4ea0cd4f40a10006196e416d89
Filip <szymanski.fp@gmail.com>
2022-01-02 21:47:33 +0100

    added save that saves the passed User object

--- src/main/java/eu/filip/notes/service/UserRepository.java
+++ src/main/java/eu/filip/notes/service/UserRepository.java
@@ -1,6 +1,8 @@
 package eu.filip.notes.service;
 
+import eu.filip.notes.model.RegistrationData;
 import eu.filip.notes.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
@@ -8,4 +10,6 @@ import java.util.ArrayList;
 public interface UserRepository extends CrudRepository<User, Long> {
     ArrayList<User> findAll();
     User findByUsername(String username);
+
+    User save(User user);
 }
