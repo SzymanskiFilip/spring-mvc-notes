f1561b548e9f5b52e14ab51ba652a4de448e7db2
Filip <szymanski.fp@gmail.com>
2022-01-02 21:47:17 +0100

    changed id to insertable = false, so saving ignores id input

--- src/main/java/eu/filip/notes/model/User.java
+++ src/main/java/eu/filip/notes/model/User.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", insertable = false)
     private Long id;
     private String username;
     private String password;
