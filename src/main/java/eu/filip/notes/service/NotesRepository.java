6bc1c74d2a2945c31c2abd1032d24e5736db413a
Filip <szymanski.fp@gmail.com>
2022-01-02 14:58:55 +0100

    fixed repo by doing a custom query

--- src/main/java/eu/filip/notes/service/NotesRepository.java
+++ src/main/java/eu/filip/notes/service/NotesRepository.java
@@ -1,10 +1,13 @@
 package eu.filip.notes.service;
 
 import eu.filip.notes.model.Note;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
 public interface NotesRepository extends CrudRepository<Note, Long> {
     ArrayList<Note> findAll();
+    @Query(value = "SELECT * FROM notes WHERE user_id = ?1", nativeQuery = true)
+    ArrayList<Note> findByUserID(Long id);
 }
