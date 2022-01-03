1ac639670141f01db0f89363e15e22853587064a
Filip <szymanski.fp@gmail.com>
2022-01-03 21:25:21 +0100

    deleted unused imports

--- src/main/java/eu/filip/notes/service/UserRepository.java
+++ src/main/java/eu/filip/notes/service/UserRepository.java
@@ -1,8 +1,6 @@
 package eu.filip.notes.service;
 
-import eu.filip.notes.model.RegistrationData;
 import eu.filip.notes.model.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
@@ -10,6 +8,5 @@ import java.util.ArrayList;
 public interface UserRepository extends CrudRepository<User, Long> {
     ArrayList<User> findAll();
     User findByUsername(String username);
-
     User save(User user);
 }
