e40fcad5a33fa636236c0418da08fe6bd6bb2db3
Filip <szymanski.fp@gmail.com>
2022-01-03 23:05:43 +0100

    created custom query that deletes based on note id AND userID that is currently logged in to prevent random deletions

--- src/main/java/eu/filip/notes/service/NotesRepository.java
+++ src/main/java/eu/filip/notes/service/NotesRepository.java
@@ -1,15 +1,19 @@
 package eu.filip.notes.service;
 
 import eu.filip.notes.model.Note;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 
 public interface NotesRepository extends CrudRepository<Note, Long> {
-    ArrayList<Note> findAll();
-
     @Query(value = "SELECT * FROM notes WHERE user_id = ?1", nativeQuery = true)
     ArrayList<Note> findByUserID(Long id);
-    void deleteById(Long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM notes WHERE id = ?1 AND user_id = ?2", nativeQuery = true)
+    void deleteByIdAndUser_id(Long id, Long user_id);
 }
